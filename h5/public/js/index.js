19acb7a9aafa76807fe8dc8098abb98640d19760
张天宇 <91193003@qq.com>
2019-11-14 03:09:35 +0800

    111

--- h5/public/js/index.js
+++ h5/public/js/index.js
@@ -15,11 +15,28 @@ $(function(){
         for(var i = 0; i <data.length;i++){
             if(i == 0){
                 $('#carousel-ol').append("<li class='active' data-target='#mycarousel', data-slide-to=" + i + "></li>");
-                $('#carousel-div').append("<div class='item active'><img class='carousel-img' src="+data[i].img_url +"></div>")
+                $('#carousel-div').append("<div class='item active'><img class='carousel-img' src="+data[i].img_url +"><div class='container'>" +
+                    " <div class='carousel-caption'>" +
+                    "   <h1>" + data[i].article_tittle +"</h1>" +
+                    "       <p>" +
+                    "           <button class='btn btn-default'>查看详情</button>" +
+                    "       </p>" +
+                    " </div>" +
+                    "" +
+                    "</div></div>")
             }else{
 
                 $('#carousel-ol').append("<li data-target='#mycarousel', data-slide-to=" + i + "></li>");
-                $('#carousel-div').append("<div class=item><img class='carousel-img' src=" + data[i].img_url + "></div>")
+                $('#carousel-div').append("<div class=item><img class='carousel-img' src=" + data[i].img_url + ">" +
+                    "   <div class='container'>" +
+                    "       <div class='carousel-caption'>" +
+                    "           <h1>" + data[i].article_tittle +"</h1>" +
+                    "       <p>" +
+                    "           <button class='btn btn-default'>查看详情</button>" +
+                    "       </p>" +
+                    "       </div>" +
+                    "   </div>" +
+                    "</div>")
             }
         }
 
