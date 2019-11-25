19e7b6db5c46809a657387e95e4a86ab65782765
zzz <911930036@qq.com>
2019-11-25 18:09:27 +0800

    修改导航栏样式

--- h5/app.js
+++ h5/app.js
@@ -7,6 +7,7 @@ var $ = require('jquery');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var contactRouter = require('./routes/contact');
+var productRouter = require('./routes/product');
 var app = express();
 
 // view engine setup
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/contact',contactRouter);
+app.use( '/product',productRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- h5/public/js/footer.js
+++ h5/public/js/footer.js
@@ -1,16 +1,29 @@
-$.get('/users/findAllDict',function (data) {
-    for(var i = 0; i < data.length; i++){
-        //加载公司名称
-        if(data[i].code == 'cname'){
-            $('#footer-cname-li').prepend(data[i].name)
+$(function() {
+    $.get('/users/findAllDict', function (data) {
+        for (var i = 0; i < data.length; i++) {
+            //加载公司名称
+            if (data[i].code == 'cname') {
+                $('#footer-cname-li').prepend(data[i].name)
+            }
+            //联系方式
+            if (data[i].code == 'tel') {
+                $('#footer-tel-a').append(data[i].name);
+            }
+            //地址
+            if (data[i].code == 'address') {
+                $('#footer-address-li').text(data[i].name);
+            }
         }
-        //联系方式
-        if(data[i].code == 'tel'){
-            $('#footer-tel-a').append(data[i].name);
+    });
+    //加载品牌
+    $.get('/users/findAllBrand',function (data) {
+        for(var i = 0; i < data.length;i++){
+            // $('#all-product').append("<div class='product-div' branid='"+ data[i].id+"'><img class='logo-div' src='../img/branlog.png'/></div>");
+            $('#footer-bran-ul').prepend("<li><a>"+ data[i].classify_name +"</a></li>");
         }
-        //地址
-        if(data[i].code == 'address'){
-            $('#footer-address-li').text(data[i].name);
-        }
-    }
-});
+        // //品牌点击事件
+        // $(".product-div").click(function () {
+        //     alert($(this).attr('branid'));
+        // });
+    });
+})
