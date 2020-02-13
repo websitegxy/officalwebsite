d682c4c0caad58ac1e14f257777755b236cb4f32
zzz <911930036@qq.com>
2020-02-13 15:13:02 +0800

    允许跨域

--- h5/app.js
+++ h5/app.js
@@ -10,7 +10,14 @@ var contactRouter = require('./routes/contact');
 var productRouter = require('./routes/product');
 var productItemRouter = require('./routes/productItem.js');
 var app = express();
-
+//允许跨域
+app.all('',function (req,res,next) {
+  res.header('Access-Control-Allow-Origin','');
+  res.header('Access-Control-Allow-Headers','*');
+  res.header('Access-Control-Allow-Methods','*');
+  //res.header('Content-Type','application/json;charset=utf-8');
+  next();
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -41,13 +48,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-//允许跨域
-app.all('',function (req,res,next) {
-  res.header('Access-Control-Allow-Origin','');
-  res.header('Access-Control-Allow-Headers','*');
-  res.header('Access-Control-Allow-Methods','*');
-  //res.header('Content-Type','application/json;charset=utf-8');
-  next();
-});
+
 
 module.exports = app;
