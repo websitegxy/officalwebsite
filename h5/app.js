706e0e99ea59785eb60390552df213278116c61d
zzz <911930036@qq.com>
2020-02-13 15:21:25 +0800

    允许跨域

--- h5/app.js
+++ h5/app.js
@@ -11,9 +11,9 @@ var productRouter = require('./routes/product');
 var productItemRouter = require('./routes/productItem.js');
 var app = express();
 //允许跨域
-app.all('',function (req,res,next) {
+app.all('*',function (req,res,next) {
   res.header('Access-Control-Allow-Origin','*');
-  res.header('Access-Control-Allow-Headers','*');
+  //res.header('Access-Control-Allow-Headers','*');
   res.header('Access-Control-Allow-Methods','*');
   //res.header('Content-Type','application/json;charset=utf-8');
   next();
